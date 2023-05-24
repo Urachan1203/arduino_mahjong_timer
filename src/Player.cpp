cd1a5793646dbe9536b6adb5effd7d2631ae92a3
Urachan0319 <urachan@ucl.nuee.nagoya-u.ac.jp>
2023-05-24 23:34:22 +0900

    add code for debug

--- src/Player.cpp
+++ src/Player.cpp
@@ -12,6 +12,7 @@ Player::Player(){
 
 Player::Player(char* player_name, int time_remain_sec, uint8_t addr){
     this->PlayerLcdInit(addr);
+    Serial.println("init");
     this->SetPlayerName(player_name);
     this->SetTimeRemainSec(time_remain_sec);
 }
@@ -22,15 +23,19 @@ LiquidCrystal_I2C Player::GetPlayerLcd(){
 
 void Player::PlayerLcdInit(uint8_t addr){
 
-    Serial.println(addr);
-    this->player_lcd = LiquidCrystal_I2C(addr, 16, 2);
+    LiquidCrystal_I2C lcd = LiquidCrystal_I2C(addr, 16, 2);
+    this->player_lcd = lcd;
     this->player_lcd.init();
     this->player_lcd.backlight();
     this->player_lcd.clear();
     this->player_lcd.setCursor(0, 0);
     this->player_lcd.print("Successfully");
     this->player_lcd.setCursor(0, 1);
-    this->player_lcd.print("Initialized!");    
+    this->player_lcd.print("Initialized!");
+    Serial.print("initializing ");
+    Serial.println(addr);
+    Serial.print("addr : ");
+    Serial.println((long) &(this->player_lcd));
     
     return;
 }
