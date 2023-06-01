eac2a7cb568d1ac20425007b0552e968eb398c78
Urachan0319 <urachan@ucl.nuee.nagoya-u.ac.jp>
2023-06-01 10:32:53 +0900

    remove unnecessary code

--- src/mahjong_timer.ino
+++ src/mahjong_timer.ino
@@ -16,31 +16,17 @@ void SystemInit(){
 
 void setup(){
   SystemInit();
-  
   ms = MahjongSetting();
-  DialogManager dm = DialogManager(); 
-
-  // create and display setting dialog 
   MahjongSettingDialog msd = MahjongSettingDialog(&ms, 0);
-  msd.DoSetting(&dm);
-
+  msd.DoSetting();
 }
 
 void loop(){
-  // TODO : 親プレイヤーの選択を行う（PlayerSelectorDialog を作る？順番割り込みの実装で使い回せそう）
-
-  // resetされたらここからスタートする
   Timer::Init(0, &ms);
-
   bool count_started = false;
+  
   while(1){    
-     // TODO : 適当なステータスコードを返す
-     // 0 : カウント継続 -> while を回り続ける
-     // 1 : pause -> while の中で止まる
-     // 2 : reset timer -> break
-     // 3 : go setting -> break & show setting dialog
-    if(TimerCommand::Reset == Timer::CountTime(count_started)) break;
+    if(TimerStatus::Reset == Timer::CountTime(count_started)) break;
     count_started = true;
   }
-
 }
